e82be122cc540bbbd2b87ed28eff4bf26999872e
Yampoy <yampoy.elvis@gmail.com>
2025-03-31 14:38:17 +0300

    Done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json()) // Convert the response to JSON
+    .then(jsonData => renderBooks(jsonData)); // Pass the JSON data to renderBooks()
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
